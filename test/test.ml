266152f243fcc17e497dfb483cf23c10f3167853
Reed Mullanix <reedmullanix@gmail.com>
2021-08-05 13:30:59 -0700

    test unicode

--- test/test.ml
+++ test/test.ml
@@ -18,8 +18,11 @@ let () =
   let lib = Library.init () in
   let face = Face.create lib "/Users/reedmullanix/Library/Fonts/iosevka-fixed-thin.ttf" 0 in
   let _ = Face.set_char_size face 0L (Int64.mul 16L 64L) 300 300 in
-  let glyph_index = Face.get_char_index face (Int64.of_int @@ Char.code 'a') in
+  (* 0x2115 is the character code for ℕ *)
+  let glyph_index = Face.get_char_index face (Int64.of_int 0x2115) in
   let _ = Face.load_glyph face glyph_index [] in
   let _ = Glyph.render (Face.glyph face) Normal in
   let bitmap = Glyph.bitmap (Face.glyph face) in
-  write_ppm "./test.ppm" (Bitmap.width bitmap) (Bitmap.height bitmap) (Bitmap.bytes bitmap)
+  write_ppm "./test.ppm" (Bitmap.width bitmap) (Bitmap.height bitmap) (Bitmap.bytes bitmap);
+  Face.close face;
+  Library.close lib
